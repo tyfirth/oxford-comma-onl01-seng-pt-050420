4c5ad0999a5c816d5572a692412276cb7cd7bcd5
Tyler Firth <tyfirth@gmail.com>
2020-05-12 01:30:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  
+if array.length == 1 
+  return "#{array[0]}"
+  
+elsif array.length == 2 
+return "#{array[0]} and #{array[1]}"
 
+ elsif array.length >= 3
+ new_last_element = "and #{array[-1]}"
+ array.pop
+ array.push(new_last_element)
+ return array.join(", ")
+ 
+ end
 end
+
