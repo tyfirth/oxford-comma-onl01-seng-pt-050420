112d6a65a351c567f714423b8f20f5e1e9e5fd01
Tyler Firth <tyfirth@gmail.com>
2020-05-12 01:15:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ if array.length == 1
 elsif array.length == 2 
 return "#{array[0]} and #{array[1]}"
 
- elsif array >= 3
+ elsif array.length >= 3
  new_last_element = "and #{array[-1]}"
  array.pop
  array.push(new_last_element)
